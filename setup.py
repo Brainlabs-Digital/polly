154864c7625c6656812f0de43cafc049c011bc2b
Tom Anthony <git@tomanthony.co.uk>
2015-07-30 15:34:44 +0100

    Bump version to 0.5.9.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='polly',
     packages=['polly'],
-    version='0.5.7',
+    version='0.5.9',
     description='A library for parsing and validating rel-alternate-hreflang entries on a page.',
     author='Tom Anthony',
     author_email='tom.anthony@distilled.net',
     url='https://github.com/DistilledLtd/polly',
-    download_url='https://github.com/DistilledLtd/polly/tarball/0.5.7',
+    download_url='https://github.com/DistilledLtd/polly/tarball/0.5.9',
     keywords=['hreflang', 'rel-alternate-hreflang'],
     classifiers=[],
     install_requires=[
